f489826807b167f069667895399be39ddd6b3e07
Nikita Kovaliov <nikita@maizy.ru>
2016-11-25 13:55:33 +0300

    track is Slack app open or not

--- dislack/__init__.py
+++ dislack/__init__.py
@@ -10,6 +10,7 @@ import subprocess
 logger = logging.getLogger('dislack')
 
 RESOURCES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources'))
+MESSAGE = 'Time to read Slack!'
 
 
 def parse_args(argv):
@@ -17,9 +18,11 @@ def parse_args(argv):
     parser.add_argument('-s', '--sleep', help='sleep time in minutes', type=int, default=15)
     parser.set_defaults(
         open_path='/usr/bin/open',
+        pgrep_path='/usr/bin/pgrep',
         slack_path='/Applications/Slack.app',
         slack_icon=os.path.join(RESOURCES_DIR, 'slack_icon.png'),
-        terminal_notifier_path='/usr/local/bin/terminal-notifier'
+        terminal_notifier_path='/usr/local/bin/terminal-notifier',
+        slack_wait_timeout=15,
     )
     return parser.parse_args(argv)
 
@@ -38,38 +41,77 @@ def send_notification(app_options):
             app_options.terminal_notifier_path,
             '-appIcon', app_options.slack_icon,
             '-title', 'diSlack',
-            '-message', 'Time to read Slack!',
+            '-message', MESSAGE,
         ])
     except subprocess.CalledProcessError:
         return False
     return True
 
 
+def is_slack_open(app_options):
+    try:
+        output = subprocess.check_output([
+            app_options.pgrep_path,
+            '-f',
+            '{}/Contents/MacOS/Slack'.format(app_options.slack_path),
+        ])
+        output = output.strip('\n ')
+        if output.isdigit():
+            return True
+        return False
+    except subprocess.CalledProcessError:
+        return False
+
+
+def wait_for_slack_closed(app_options):
+    while is_slack_open(app_options):
+        try:
+            time.sleep(app_options.slack_wait_timeout)
+        except KeyboardInterrupt:
+            print()
+            break
+    return True
+
+
+def wait_and_notice(app_options):
+    logger.info('You will notice in {} minutes.'.format(app_options.sleep))
+    logger.info('Press Ctrl+C if you want to skip waiting and open Slack.')
+    minute = 0
+    skipped = False
+    try:
+        while minute < app_options.sleep:
+            time.sleep(60)
+            minute += 1
+            logger.debug('.')
+    except KeyboardInterrupt:
+        print()
+        logger.info('Waiting skipped')
+        skipped = True
+    if not skipped:
+        logger.info(MESSAGE)
+        open_slack(app_options)
+        send_notification(app_options)
+    else:
+        logger.info('Now you have 3 seconds to press Ctrl+C for exiting diSlack or else I\'ll launch Slack for you')
+        try:
+            time.sleep(3)
+        except KeyboardInterrupt:
+            print()
+            return False
+        open_slack(app_options)
+    return True
+
+
 def main(argv):
     app_options = parse_args(argv)
     while True:
-        minute = 0
-        logger.info('sleeping for {} minutes'.format(app_options.sleep))
-        skipped = False
-        try:
-            while minute < app_options.sleep:
-                time.sleep(60)
-                minute += 1
-                logger.debug('.')
-        except KeyboardInterrupt:
-            logger.info('skipping')
-            skipped = True
-        if not skipped:
-            logger.info('time to read Slack')
-            open_slack(app_options)
-            send_notification(app_options)
-        else:
-            logger.info('Now you have 3 seconds to press Ctrl+C for exiting diSlack or else I\'ll launch Slack for you')
-            try:
-                time.sleep(3)
-            except KeyboardInterrupt:
-                break
-            open_slack(app_options)
+        if is_slack_open(app_options):
+            logger.info('Slack is running')
+            logger.info('Send "/dnd {}m", close Slack, then I start to notify you'.format(app_options.sleep))
+        wait_for_slack_closed(app_options)
+        need_to_continue = wait_and_notice(app_options)
+        if not need_to_continue:
+            break
+
     logger.info('buy')
     return True
-
